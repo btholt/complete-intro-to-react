6c59a3ee812048ab895501f2361647f1ec2113b6
Brian Holt <btholt@gmail.com>
2016-03-06 20:55:42 -0800

    Delete everything for the start branch

--- js/BrowserEntry.jsx
+++
@@ -1,5 +0,0 @@
-const React = require('react')
-const ReactDOM = require('react-dom')
-const App = require('./ClientApp')
-
-ReactDOM.render(<App />, document.getElementById('app'))
--- test/helpers/setup.js
+++
@@ -1,6 +0,0 @@
-require('babel-register')
-require('babel-polyfill')
-
-global.document = require('jsdom').jsdom('<body><div id="app"></div></body>')
-global.window = document.defaultView
-global.navigator = window.navigator
