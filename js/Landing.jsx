2735c5ff352cb6b20bb83336611bfcf909fcdbfb
Brian Holt <btholt@gmail.com>
2016-03-24 14:17:22 -0500

    FEM step 10

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,11 +1,12 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 const Landing = () => (
   <div className='app-container'>
     <div className='home-info'>
       <h1 className='title'>svideo</h1>
       <input className='search' type='text' placeholder='Search' />
-      <button className='browse-all'> or Browse All</button>
+      <Link to='/search' className='browse-all'> or Browse All</Link>
     </div>
   </div>
 )
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,21 @@
 const React = require('react')
-const shows = require('../public/data')
+const data = require('../public/data')
 
 const Search = () => (
-  <pre><code>
-    {JSON.stringify(shows, null, 4)}
-  </code></pre>
+  <div className='container'>
+    <div className='shows'>
+      {data.shows.map((show) => (
+        <div className='show-card'>
+          <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
+          <div className='show-card-text'>
+            <h3 className='show-card-title'>{show.title}</h3>
+            <h4 className='show-card-year'>({show.year})</h4>
+            <p className='show-card-description'>{show.description}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 )
 
 module.exports = Search
