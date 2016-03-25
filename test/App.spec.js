4c79be278535c23124a632ee7c7c618d70664564
Brian Holt <btholt@gmail.com>
2016-03-25 10:19:47 -0500

    FEM step 15

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,9 +1,37 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const React = require('react')
+const Search = require('../js/Search')
+const ShowCard = require('../js/ShowCard')
+const { shallow, mount } = require('enzyme')
+const { shows } = require('../public/data')
 
 describe('<Search /> ', () => {
-  it('should pass', () => {
-    expect(1 + 1).to.equal(2)
+  it('should render the brand', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.contains(<h1 className='brand'>svideo</h1>)).to.be.true
+  })
+
+  it('should render as many shows as there are data for', () => {
+    const wrapper = shallow(<Search />)
+    expect(wrapper.find(ShowCard).length).to.equal(shows.length)
+  })
+
+  it('should filter correctly given new state', () => {
+    const wrapper = mount(<Search />)
+    const input = wrapper.find('.search-input')
+    input.node.value = 'house'
+    input.simulate('change')
+    expect(wrapper.state('searchTerm')).to.equal('house')
+    expect(wrapper.find('.show-card').length).to.equal(2)
   })
 })
+
+
+
+
+
+
+
+
