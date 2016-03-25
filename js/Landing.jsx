64d08ffbeb5f599258d24e774f50746cc64acf26
Brian Holt <btholt@gmail.com>
2016-03-25 14:44:09 -0500

    FEM step 22

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,12 +1,40 @@
 const React = require('react')
+const { hashHistory } = require('react-router')
 const { Link } = require('react-router')
+const { connector } = require('./Store')
 
-const Landing = () => (
-  <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <Link to='/search' className='browse-all'> or Browse All</Link>
-  </div>
-)
+class Landing extends React.Component {
+  constructor (props) {
+    super(props)
 
-module.exports = Landing
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+    this.gotoSearch = this.gotoSearch.bind(this)
+  }
+  handleSearchTermEvent (event) {
+    this.props.setSearchTerm(event.target.value)
+  }
+  gotoSearch (event) {
+    hashHistory.push('search')
+    event.preventDefault()
+  }
+  render () {
+    return (
+      <div className='home-info'>
+        <h1 className='title'>svideo</h1>
+        <form onSubmit={this.gotoSearch}>
+          <input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='search' type='text' placeholder='Search' />
+        </form>
+        <Link to='/search' className='browse-all'> or Browse All</Link>
+      </div>
+    )
+  }
+}
+
+const { func, string } = React.PropTypes
+
+Landing.propTypes = {
+  searchTerm: string,
+  setSearchTerm: func
+}
+
+module.exports = connector(Landing)
--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,14 +1,17 @@
 const React = require('react')
+const { Link } = require('react-router')
 
 const ShowCard = (props) => (
-  <div className='show-card'>
-    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
-    <div className='show-card-text'>
-      <h3 className='show-card-title'>{props.title}</h3>
-      <h4 className='show-card-year'>({props.year})</h4>
-      <p className='show-card-description'>{props.description}</p>
+  <Link to={`/details/${props.imdbID}`}>
+    <div className='show-card'>
+      <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+      <div className='show-card-text'>
+        <h3 className='show-card-title'>{props.title}</h3>
+        <h4 className='show-card-year'>({props.year})</h4>
+        <p className='show-card-description'>{props.description}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 )
 
 const { string } = React.PropTypes
@@ -17,7 +20,8 @@ ShowCard.propTypes = {
   title: string.isRequired,
   description: string.isRequired,
   year: string.isRequired,
-  poster: string.isRequired
+  poster: string.isRequired,
+  imdbID: string.isRequired
 }
 
 module.exports = ShowCard
