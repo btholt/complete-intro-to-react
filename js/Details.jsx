5ff5ce42c216f700740a8d50f8c5d06e346d6184
Brian Holt <btholt@gmail.com>
2016-03-25 11:47:26 -0500

    FEM step 18

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,11 +2,22 @@ const React = require('react')
 
 class Details extends React.Component {
   render () {
+    const params = this.props.params || {}
+    const { title, description, year, poster, trailer } = params
     return (
-      <div style={{textAlign: 'left'}} className='container'>
-        <pre><code>
-          {JSON.stringify(this.props.params, null, 4)}
-        </code></pre>
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+        </header>
+        <div className='video-info'>
+          <h1 className='video-title'>{title}</h1>
+          <h2 className='video-year'>({year})</h2>
+          <img className='video-poster' src={`public/img/posters/${poster}`} />
+          <p className='video-description'>{description}</p>
+        </div>
+        <div className='video-container'>
+          <iframe src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&amp;showinfo=0`} frameBorder='0' allowFullScreen></iframe>
+        </div>
       </div>
     )
   }
