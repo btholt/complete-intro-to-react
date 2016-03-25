c4b0dc296380f2fba83bdc022553d8199533bcdc
Brian Holt <btholt@gmail.com>
2016-03-25 16:34:21 -0500

    FEM step 26

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,20 +1,42 @@
 const React = require('react')
 const Header = require('./Header')
+const axios = require('axios')
 const { connector } = require('./Store')
 
 class Details extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      omdbData: {}
+    }
+  }
+  componentDidMount () {
+    axios.get(`http://www.omdbapi.com/?i=${this.assignShow(this.props.params.id).imdbID}`)
+      .then((response) => {
+        this.setState({omdbData: response.data})
+      })
+      .catch((error) => {
+        console.error('axios error', error)
+      })
+  }
   assignShow (id) {
     const showArray = this.props.shows.filter((show) => show.imdbID === id)
     return showArray[0]
   }
   render () {
     const { title, description, year, poster, trailer } = this.assignShow(this.props.params.id)
+    let rating
+    if (this.state.omdbData.imdbRating) {
+      rating = <h3 className='video-rating'>{this.state.omdbData.imdbRating}</h3>
+    }
     return (
       <div className='container'>
         <Header />
         <div className='video-info'>
           <h1 className='video-title'>{title}</h1>
           <h2 className='video-year'>({year})</h2>
+          {rating}
           <img className='video-poster' src={`/public/img/posters/${poster}`} />
           <p className='video-description'>{description}</p>
         </div>
