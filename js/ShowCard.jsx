7cdd80189a9f0c7d71e3031580a9e73cf8b0d11e
Michael Friedman <contact@michaeldavidfriedman.com>
2016-09-05 10:26:06 -0700

    60% through React intro course. First problem, mocha does not seem to be running sample test.

---
+++ js/ShowCard.jsx
@@ -0,0 +1,23 @@
+const React = require('react')
+
+const ShowCard = (props) => (
+  <div className='show-card'>
+    <img src={`public/img/posters/${props.poster}`} className='show-card-img' />
+    <div className='show-card-text'>
+      <h3 className='show-card-title'>{props.title}</h3>
+      <h4 className='show-card-year'>({props.year})</h4>
+      <p className='show-card-description'>{props.description}</p>
+    </div>
+  </div>
+)
+
+const { string } = React.PropTypes
+
+ShowCard.propTytpes = {
+  title: string.isRequired,
+  description: string.isRequired,
+  year: string.isRequired,
+  poster: string.isRequired
+}
+
+module.exports = ShowCard
---
+++ test/helpers/App.spec.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+
+const { expect } = require('chai')
+
+describe('<Search /> ', () => {
+  xit('should pass', () => {
+    expect(1 + 1).to.Equal(5)
+  })
+})
