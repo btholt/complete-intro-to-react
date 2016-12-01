22adfbc8b197abf463eff26564a10277b154b1bb
Brian Holt <btholt@gmail.com>
2016-12-01 11:41:31 -0600

    3

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  devtool: 'eval',
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
