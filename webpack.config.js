af78d7ab99e021f24902dc54291572eb0c53298d
Brian Holt <btholt@gmail.com>
2016-12-01 13:55:18 -0600

    6

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
   },
+  devServer: {
+    publicPath: '/public/'
+  },
   resolve: {
     extensions: ['.js', '.json']
   },
@@ -18,6 +21,12 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
       {
         include: path.resolve(__dirname, 'js'),
         test: /\.js$/,
