6cb1e0470f6c3051556b1801aa551e725020d256
Brian Holt <btholt@gmail.com>
2016-12-01 17:05:08 -0600

    12

--- js/Search.spec.js
+++ js/Search.spec.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { shallow } from 'enzyme'
+import { shallowToJson } from 'enzyme-to-json'
 import Search from './Search'
-import renderer from 'react-test-renderer'
+import ShowCard from './ShowCard'
+import preload from '../public/data.json'
 
 test('Search snapshot test', () => {
-  const component = renderer.create(<Search />)
-  const tree = component.toJSON()
+  const component = shallow(<Search />)
+  const tree = shallowToJson(component)
   expect(tree).toMatchSnapshot()
+})
+
+test('Search should render a ShowCard for each show', () => {
+  const component = shallow(<Search />)
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length)
+})
+
+test('Search should render correct amount of shows based on search', () => {
+  const searchWord = 'house'
+  const component = shallow(<Search />)
+  component.find('input').simulate('change', {target: {value: searchWord}})
+  const showCount = preload.shows.filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0).length
+  expect(component.find(ShowCard).length).toEqual(showCount)
 })
