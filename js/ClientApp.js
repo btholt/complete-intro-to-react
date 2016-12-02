fc9a6c223e034e9137d9b3c4ba134caa6c358e08
Brian Holt <btholt@gmail.com>
2016-12-02 12:22:15 -0600

    18

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter, Match } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './store'
 import Landing from './Landing'
 import Search from './Search'
 import Details from './Details'
@@ -12,20 +14,22 @@ const App = React.createClass({
   render () {
     return (
       <BrowserRouter>
-        <div className='app'>
-          <Match exactly pattern='/' component={Landing} />
-          <Match
-            pattern='/search'
-            component={(props) => <Search shows={preload.shows} {...props} />}
-          />
-          <Match
-            pattern='/details/:id'
-            component={(props) => {
-              const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
-              return <Details show={shows[0]} {...props} />
-            }}
-          />
-        </div>
+        <Provider store={store}>
+          <div className='app'>
+            <Match exactly pattern='/' component={Landing} />
+            <Match
+              pattern='/search'
+              component={(props) => <Search shows={preload.shows} {...props} />}
+            />
+            <Match
+              pattern='/details/:id'
+              component={(props) => {
+                const shows = preload.shows.filter((show) => props.params.id === show.imdbID)
+                return <Details show={shows[0]} {...props} />
+              }}
+            />
+          </div>
+        </Provider>
       </BrowserRouter>
     )
   }
--- js/Landing.js
+++ js/Landing.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Link } from 'react-router'
+const { string } = React.PropTypes
 
 const Landing = React.createClass({
+  propTypes: {
+    searchTerm: string
+  },
   render () {
     return (
       <div className='landing'>
         <h1>svideo</h1>
-        <input type='text' placeholder='Search' />
+        <input value={this.props.searchTerm} type='text' placeholder='Search' />
         <Link to='/search'>or Browse All</Link>
       </div>
     )
   }
 })
 
-export default Landing
+const mapStateToProps = (state) => {
+  return {
+    searchTerm: state.searchTerm
+  }
+}
+
+export default connect(mapStateToProps)(Landing)
