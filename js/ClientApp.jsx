2021dc62ff7b61f9476baf2339aa2a94705e571d
Brian Holt <btholt@gmail.com>
2017-05-30 14:23:44 -0500

    5

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
 const App = () => (
-  <div className="app">
-    <div className="landing">
-      <h1>svideo</h1>
-      <input type="text" placeholder="Search" />
-      <a>or Browse All</a>
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+      <Route path="/search" component={Search} />
     </div>
-  </div>
+  </HashRouter>
 );
 
 render(<App />, document.getElementById('app'));
---
+++ js/Search.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search = () => <h1>hi lol this is search</h1>;
+
+export default Search;
