427f370f3b6e3fe666186bf096b5c7b09d6b8680
Brian Holt <btholt@gmail.com>
2017-05-30 14:07:12 -0500

    4

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
   },
+  devServer: {
+    publicPath: '/public/'
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   },
