3f110517b2a2694df398fecfd524da6d78444087
Brian Holt <btholt@gmail.com>
2017-05-30 14:32:33 -0500

    6

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
+const FourOhFour = () => <h1>404</h1>;
+
 const App = () => (
-  <HashRouter>
+  <BrowserRouter>
     <div className="app">
-      <Route exact path="/" component={Landing} />
-      <Route path="/search" component={Search} />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route path="/search" component={Search} />
+        <Route component={FourOhFour} />
+      </Switch>
     </div>
-  </HashRouter>
+  </BrowserRouter>
 );
 
 render(<App />, document.getElementById('app'));
