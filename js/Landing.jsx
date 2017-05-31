d347951c95c544bfaf8be17de5b860f87b2d8356
Brian Holt <btholt@gmail.com>
2017-05-31 17:07:44 -0500

    21

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,17 +1,38 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import type { RouterHistory } from 'react-router-dom';
 import { setSearchTerm } from './actionCreators';
 
-const Landing = (props: { searchTerm: string, handleSearchTermChange: Function }) => (
-  <div className="landing">
-    <h1>{props.searchTerm}</h1>
-    <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
-    <Link to="/search">or Browse All</Link>
-  </div>
-);
+class Landing extends Component {
+  props: {
+    searchTerm: string,
+    handleSearchTermChange: Function,
+    history: RouterHistory
+  };
+  goToSearch = (event: SyntheticEvent) => {
+    event.preventDefault();
+    this.props.history.push('/search');
+  };
+  render() {
+    return (
+      <div className="landing">
+        <h1>svideo</h1>
+        <form onSubmit={this.goToSearch}>
+          <input
+            onChange={this.props.handleSearchTermChange}
+            value={this.props.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </form>
+        <Link to="/search">or Browse All</Link>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({ searchTerm: state.searchTerm });
 const mapDispatchToProps = (dispatch: Function) => ({
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,34 +1,26 @@
 // @flow
 
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
 import ShowCard from './ShowCard';
 import Header from './Header';
 
-class Search extends Component {
-  state = {
-    searchTerm: ''
-  };
-  props: {
-    shows: Array<Show>
-  };
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({ searchTerm: event.target.value });
-  };
-  render() {
-    return (
-      <div className="search">
-        <Header searchTerm={this.state.searchTerm} showSearch handleSearchTermChange={this.handleSearchTermChange} />
-        <div>
-          {this.props.shows
-            .filter(
-              show =>
-                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
-            )
-            .map(show => <ShowCard key={show.imdbID} {...show} />)}
-        </div>
-      </div>
-    );
-  }
-}
+const Search = (props: {
+  searchTerm: string, // eslint-disable-line react/no-unused-prop-types
+  shows: Array<Show>
+}) => (
+  <div className="search">
+    <Header showSearch />
+    <div>
+      {props.shows
+        .filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(props.searchTerm.toUpperCase()) >= 0)
+        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+    </div>
+  </div>
+);
 
-export default Search;
+const mapStateToProps = state => ({
+  searchTerm: state.searchTerm
+});
+
+export default connect(mapStateToProps)(Search);
