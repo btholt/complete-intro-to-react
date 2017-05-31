eb741b8266a5558db3a83976446ad4bddaca99ef
Brian Holt <btholt@gmail.com>
2017-05-31 14:59:44 -0500

    19

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -2,14 +2,17 @@
 
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-const Wrapper = styled.div`
+const Wrapper = styled((Link: any))`
   width: 32%;
   border: 2px solid #333;
   border-radius: 4px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -18,8 +21,8 @@ const Image = styled.img`
   margin-right: 10px;
 `;
 
-const ShowCard = (props: { poster: string, title: string, year: string, description: string }) => (
-  <Wrapper>
+const ShowCard = (props: Show) => (
+  <Wrapper to={`/details/${props.imdbID}`}>
     <Image alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
     <div>
       <h3>{props.title}</h3>
