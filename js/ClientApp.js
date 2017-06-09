982b7510b9aae40e8832074b5dfd6533f11a91de
Erinç Emer <erincemer@gmail.com>
2017-06-08 18:56:38 -0700

    ssa

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,12 +2,26 @@ var div = React.DOM.div
 var h1 = React.DOM.h1
 
 var MyTitle = React.createClass({
-
+  render() {
+    return (
+      div(null,  //we dont have to have a div here
+        h1({style: {color: this.props.color}}, this.props.title)
+        ))
+  }
 })
+
+var MyTitleFact = React.createFactory(MyTitle)
+var ce = React.createElement
+
+//we can use myTitle many times in MyFirstComponnent
+//React.createElement(MyTitle, null) and MyTitleFact(null) and
+// ce(MyTitle, null) are the same
 
 var MyFirstComponent = (
     div(null,
-        h1(null, 'This is my first component!')
+      MyTitleFact({title: 'Props are great!', color: "rebeccapurple"}), // MyTitleFact(null)
+      React.createElement(MyTitle, {title: 'Use props everywhere!', color: "mediumaquamarine"}), //React.createElement(MyTitle, null)
+      ce(MyTitle, {title: 'Props are the best!', color: "peru"}) // ce(MyTitle, null)
     )
 )
 
