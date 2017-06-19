8016968177017d407a18abfd9ab23c124ac65d8b
Brian Holt <btholt@gmail.com>
2017-06-19 14:52:40 -0700

    6

--- src/ShowCard.jsx
+++ src/ShowCard.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { string } from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 32%;
+  border: 2px solid #333;
+  border-radius: 5px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
+`;
 
 const ShowCard = props =>
-  <div className="show-card">
-    <img src={`/public/img/posters/${props.poster}`} alt={`${props.title} show poster`} />
+  <Wrapper className="show-card">
+    <Image src={`/public/img/posters/${props.poster}`} alt={`${props.title} show poster`} />
     <div>
       <h3>{props.title}</h3>
       <h4>({props.year})</h4>
       <p>{props.description}</p>
     </div>
-  </div>;
+  </Wrapper>;
 
 ShowCard.propTypes = {
   poster: string.isRequired,
