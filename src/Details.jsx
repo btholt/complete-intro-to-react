dd807fa82fd8500c7c48315e28fcc3eca851d6ef
Brian Holt <btholt@gmail.com>
2017-06-20 11:28:02 -0700

    12

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,32 +1,55 @@
 import React from 'react';
 import { string } from 'prop-types';
+import axios from 'axios';
 import Header from './Header';
+import Spinner from './Spinner';
 
-const Details = props =>
-  <div className="details">
-    <Header />
-    <section>
-      <h1>{props.title}</h1>
-      <h2>({props.year})</h2>
-      <img src={`/public/img/posters/${props.poster}`} alt={`Poster for ${props.title}`} />
-      <p>{props.description}</p>
-    </section>
-    <div>
-      <iframe
-        src={`https://www.youtube-nocookie.com/embed/${props.trailer}?rel=0&amp;showinfo=0&amp;controls=0`}
-        frameBorder="0"
-        allowFullScreen
-        title={`Trailer for ${props.title}`}
-      />
-    </div>
-  </div>;
-
-Details.propTypes = {
-  title: string.isRequired,
-  description: string.isRequired,
-  poster: string.isRequired,
-  trailer: string.isRequired,
-  year: string.isRequired
-};
+class Details extends React.Component {
+  static propTypes = {
+    title: string.isRequired,
+    description: string.isRequired,
+    poster: string.isRequired,
+    trailer: string.isRequired,
+    year: string.isRequired,
+    imdbID: string.isRequired
+  };
+  state = {
+    apiData: {}
+  };
+  componentDidMount() {
+    axios.get(`http://localhost:3000/${this.props.imdbID}`).then(response => {
+      this.setState({ apiData: response.data });
+    });
+  }
+  render() {
+    let rating;
+    if (this.state.apiData.rating) {
+      rating = <h3>{this.state.apiData.rating}</h3>;
+    } else {
+      rating = <Spinner />;
+    }
+    const { title, year, poster, description, trailer } = this.props;
+    return (
+      <div className="details">
+        <Header />
+        <section>
+          <h1>{title}</h1>
+          <h2>({year})</h2>
+          {rating}
+          <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+          <p>{description}</p>
+        </section>
+        <div>
+          <iframe
+            src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;showinfo=0&amp;controls=0`}
+            frameBorder="0"
+            allowFullScreen
+            title={`Trailer for ${title}`}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
