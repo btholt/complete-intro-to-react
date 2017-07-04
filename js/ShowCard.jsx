1f0cd64f9989150adc820953c8f73c1190e8fc3c
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-04 19:22:40 -0400

    Add propTypes to ShowCard component

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
 
 const ShowCard = props => (
   <div className="show-card">
@@ -10,5 +11,14 @@ const ShowCard = props => (
     </div>
   </div>
 );
+
+ShowCard.propTypes = {
+  show: shape({
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+  }).isRequired
+};
 
 export default ShowCard;
