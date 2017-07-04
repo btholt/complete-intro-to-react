718190cf57e6d17f8cc55a66a29cefaf0d59bcaa
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-04 19:07:40 -0400

    Make ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
 
-const Search = () => <h1>hi lol this is search</h1>;
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => <ShowCard show={show} />)}
+  </div>
+);
 
 export default Search;
