6cc946480f99b3b235caf8d0c7508e99fab0337b
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-04 13:09:50 -0400

    Convert createElement to JSX

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-const ce = React.createElement;
+import React from 'react';
+import { render } from 'react-dom';
 
 const MyTitle = function MyTitle(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-}
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>
+  );
+};
 
 const MyFirstComponent = function MyFirstComponent() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  )
-}
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of Thrones" color="YellowGreen" />
+      <MyTitle title="Stranger Things" color="GreenYellow" />
+      <MyTitle title="Rick and Morty" color="burlywood" />
+      <MyTitle title="Silicon Valley" color="peru" />
+    </div>
+  );
+};
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<MyFirstComponent />, document.getElementById('app'));
