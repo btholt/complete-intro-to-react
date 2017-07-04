d12f39871173d3b4ec38ff62e66bb85bb196cb60
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-04 16:27:23 -0400

    Convert from HashRouter to BrowserRouter

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => (
   <div className="landing">
     <h1>svideo</h1>
     <input type="text" placeholder="Search" />
-    <a>or Browse All</a>
+    <Link to="/search">or Browse All</Link>
   </div>
 );
 
