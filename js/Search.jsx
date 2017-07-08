a36a5488fb71d3ac8530da12723f17e9c5d63384
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-08 17:36:39 -0400

    Add search filtering functionality

--- js/Search.jsx
+++ js/Search.jsx
@@ -24,7 +24,12 @@ class Search extends Component {
           />
         </header>
         <div>
-          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
       </div>
     );
