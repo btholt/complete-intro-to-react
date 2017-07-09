06bf052643d37451a73c35e8b934020caad4da1d
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 11:57:14 -0400

    Test then number of show cards

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import preload from '../../data.json';
 import Search from '../Search';
+import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
   const component = shallow(<Search />);
   expect(component).toMatchSnapshot();
 });
+
+test('Search should render correct amount of shows', () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+});
+
+xtest('Search should render correct amount of shows based on search term', () => {});
