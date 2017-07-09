aec007245518ee71c7bfc426e9004835619aa637
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 12:13:47 -0400

    Add test for the search field

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -14,4 +14,12 @@ test('Search should render correct amount of shows', () => {
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
-xtest('Search should render correct amount of shows based on search term', () => {});
+test('Search should render correct amount of shows based on search term', () => {
+  const searchWord = 'black';
+  const component = shallow(<Search />);
+  component.find('input').simulate('change', { target: { value: searchWord } });
+  const showCount = preload.shows.filter(show =>
+    `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
