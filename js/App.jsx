5aa145cde80587cede4d68cc4ced55301498f925
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 18:55:19 -0400

    Add url parameters

--- js/App.jsx
+++ js/App.jsx
@@ -2,8 +2,11 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
+import preload from '../data.json';
 
 const FourOhFour = () => <h1>404</h1>;
 
@@ -13,6 +16,13 @@ const App = () =>
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route
+          path="/details/:id"
+          component={(props: { match: Match }) => {
+            const selectedShow = preload.shows.find(show => props.match.params.id === show.imdbID);
+            return <Details show={selectedShow} {...props} />;
+          }}
+        />
         <Route component={FourOhFour} />
       </Switch>
     </div>
