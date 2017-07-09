bdee14e60e93bbfeda783b0dd346569585334c2c
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 17:03:41 -0400

    Defining flow-typed

--- js/App.jsx
+++ js/App.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
