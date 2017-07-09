559883cd7d6c0edd01bc6e5f81188b89fde653da
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-08 20:41:50 -0400

    Shallow testing with enzyme

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Search from '../Search';
 
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
 });
