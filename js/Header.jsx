87a552137854e2e05670cc25d43dbf6cfcbe1c1f
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 21:09:30 -0400

    Add conditional display logic for back button

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,11 +3,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = () =>
-  <header>
-    <h1>
-      <Link to="/">svideo</Link>
-    </h1>
-  </header>;
+const Header = (props: { showSearch?: boolean }) => {
+  let utilSpace;
+  if (props.showSearch) {
+    utilSpace = <h1>lol</h1>;
+  } else {
+    utilSpace = (
+      <h2>
+        <Link to="/search">Back</Link>
+      </h2>
+    );
+  }
+  return (
+    <header>
+      <h1>
+        <Link to="/">svideo</Link>
+      </h1>
+      {utilSpace}
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  showSearch: false
+};
 
 export default Header;
