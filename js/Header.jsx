cb3b208261ad468ac76302bf7ccb175b3578751e
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 20:55:46 -0400

    Create a header component

---
+++ js/Header.jsx
@@ -0,0 +1,13 @@
+// @flow
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () =>
+  <header>
+    <h1>
+      <Link to="/">svideo</Link>
+    </h1>
+  </header>;
+
+export default Header;
