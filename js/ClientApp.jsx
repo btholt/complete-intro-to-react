7b3d03385dc435113a4cf4b7e49eb006fe6fb77f
Carlo Santos <carlox7@gmail.com>
2017-07-10 14:27:04 -0700

    webpack and babel configurations

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 
 const MyTitle = function(props) {
@@ -18,4 +21,4 @@ const MyFirstComponent = function() {
   );
 };
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById('app'));
