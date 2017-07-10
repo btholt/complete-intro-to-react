daf282f3ca814cddf032822551bfc959a0d7f7ae
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-09 22:00:26 -0400

    Refactor header and search component for conditional display logic

--- js/Header.jsx
+++ js/Header.jsx
@@ -3,10 +3,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Header = (props: { showSearch?: boolean }) => {
+const Header = (props: { showSearch?: boolean, handleSearchTermChange?: Function, searchTerm?: string }) => {
   let utilSpace;
   if (props.showSearch) {
-    utilSpace = <h1>lol</h1>;
+    utilSpace = (
+      <input onChange={props.handleSearchTermChange} value={props.searchTerm} type="text" placeholder="Search" />
+    );
   } else {
     utilSpace = (
       <h2>
@@ -25,7 +27,9 @@ const Header = (props: { showSearch?: boolean }) => {
 };
 
 Header.defaultProps = {
-  showSearch: false
+  showSearch: false,
+  handleSearchTermChange: function noop() {},
+  searchTerm: ''
 };
 
 export default Header;
