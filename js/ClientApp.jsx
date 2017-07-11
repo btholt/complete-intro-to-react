84feec9e6fe1369b562ad30d3bbfbbe161827e10
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-10 21:26:12 -0400

    Add react perf tools to profile app
    Perf.stop() and Perf.printWasted()

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,7 +2,11 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import Perf from 'react-addons-perf';
 import App from './App';
+
+window.Perf = Perf;
+Perf.start();
 
 const renderApp = () => {
   render(<App />, document.getElementById('app'));
