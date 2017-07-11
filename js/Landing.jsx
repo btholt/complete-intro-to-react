390c23e01312097476ba01e26197363477e315c1
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-11 19:37:35 -0400

    Set up landing to interact with Store

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,14 +1,16 @@
 // @flow
 
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Landing = () => (
+const Landing = () =>
   <div className="landing">
     <h1>svideo</h1>
     <input type="text" placeholder="Search" />
     <Link to="/search">or Browse All</Link>
-  </div>
-);
+  </div>;
 
-export default Landing;
+const mapStateToProps = state => ({ searchTerm: state.searchTerm });
+
+export default connect(mapStateToProps)(Landing);
