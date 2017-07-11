ce5403970ce7f5d84905de5648a8db886d6b6b8c
Carlo Santos <carlox7@gmail.com>
2017-07-11 14:13:52 -0700

    working or routes

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,51 +1,63 @@
 // @flow
-
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import type { RouterHistory } from 'react-router-dom';
-import { object } from 'prop-types';
-import { setSearchTerm } from './actionCreators';
 
-class Landing extends React.Component {
-  static contextTypes = {
-    history: object
-  };
-  props: {
-    searchTerm: string,
-    handleSearchTermChange: Function,
-    history: RouterHistory
-  };
-  goToSearch = event => {
-    event.preventDefault();
-    this.props.history.push('/search');
-  };
-  render() {
-    return (
-      <div className="landing">
-        <h1>svideo</h1>
-        <form onSubmit={this.goToSearch}>
-          <input
-            onChange={this.props.handleSearchTermChange}
-            value={this.props.searchTerm}
-            type="text"
-            placeholder="Search"
-          />
-        </form>
-        <Link to="/search">or Browse All</Link>
-      </div>
-    );
-  }
-}
+const Landing = () => (
+  <div className="landing">
+    <h1>carlo video</h1>
+    <input type="text" placeholder="Search" />
+    <Link to="/search">or Browse All</Link>
+  </div>
+);
 
-const mapStateToProps = state => ({
-  searchTerm: state.searchTerm
-});
+export default Landing;
 
-const mapDispatchToProps = (dispatch: Function) => ({
-  handleSearchTermChange(event) {
-    dispatch(setSearchTerm(event.target.value));
-  }
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Landing);
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import type { RouterHistory } from 'react-router-dom';
+// import { object } from 'prop-types';
+// import { setSearchTerm } from './actionCreators';
+//
+// class Landing extends React.Component {
+//   static contextTypes = {
+//     history: object
+//   };
+//   props: {
+//     searchTerm: string,
+//     handleSearchTermChange: Function,
+//     history: RouterHistory
+//   };
+//   goToSearch = event => {
+//     event.preventDefault();
+//     this.props.history.push('/search');
+//   };
+//   render() {
+//     return (
+//       <div className="landing">
+//         <h1>svideo</h1>
+//         <form onSubmit={this.goToSearch}>
+//           <input
+//             onChange={this.props.handleSearchTermChange}
+//             value={this.props.searchTerm}
+//             type="text"
+//             placeholder="Search"
+//           />
+//         </form>
+//         <Link to="/search">or Browse All</Link>
+//       </div>
+//     );
+//   }
+// }
+//
+// const mapStateToProps = state => ({
+//   searchTerm: state.searchTerm
+// });
+//
+// const mapDispatchToProps = (dispatch: Function) => ({
+//   handleSearchTermChange(event) {
+//     dispatch(setSearchTerm(event.target.value));
+//   }
+// });
+//
+// export default connect(mapStateToProps, mapDispatchToProps)(Landing);
