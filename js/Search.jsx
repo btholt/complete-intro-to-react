b913bcddbfdb14f51997f3b66efb7ed2c1e46c22
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-11 22:47:24 -0400

    Test dispatching actions

--- js/Search.jsx
+++ js/Search.jsx
@@ -22,4 +22,5 @@ const mapStateToProps = state => ({
   searchTerm: state.searchTerm
 });
 
+export const Unwrapped = Search;
 export default connect(mapStateToProps)(Search);
--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import store from '../store';
+import { setSearchTerm } from '../actionCreators'
 import preload from '../../data.json';
-import Search from '../Search';
+import Search, { Unwrapped as UnwrappedSearch } from '../Search';
 import ShowCard from '../ShowCard';
 
 test('Search renders correctly', () => {
-  const component = shallow(<Search shows={preload.shows} />);
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
   expect(component).toMatchSnapshot();
 });
 
 test('Search should render correct amount of shows', () => {
-  const component = shallow(<Search shows={preload.shows} />);
+  const component = shallow(<UnwrappedSearch shows={preload.shows} searchTerm="" />);
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
 test('Search should render correct amount of shows based on search term', () => {
   const searchWord = 'black';
-  const component = shallow(<Search shows={preload.shows} />);
-  component.find('input').simulate('change', { target: { value: searchWord } });
+  store.dispatch(setSearchTerm(searchWord))
+  const component = render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <Search shows={preload.shows} searchTerm={searchWord} />
+      </MemoryRouter>
+    </Provider>
+  );
+
   const showCount = preload.shows.filter(
     show => `${show.title} ${show.description}`.toUpperCase().indexOf(searchWord.toUpperCase()) >= 0
   ).length;
-  expect(component.find(ShowCard).length).toEqual(showCount);
+  expect(component.find('.show-card').length).toEqual(showCount);
 });
