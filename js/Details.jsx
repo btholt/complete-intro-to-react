757e7ef91cb23e84ec95c85b0ed9f91de54a0102
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-11 22:14:05 -0400

    Add async actions to the details component

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,32 +1,31 @@
 // @flow
 
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { getAPIDetails } from './actionCreators';
 import Header from './Header';
 import Spinner from './Spinner';
 
 class Details extends Component {
-  state = {
-    apiData: { rating: '' }
-  };
-
   componentDidMount() {
-    axios.get(`http://localhost:3000/${this.props.show.imdbID}`).then((response: { data: { rating: string } }) => {
-      this.setState({ apiData: response.data });
-    });
+    if (!this.props.rating) {
+      this.props.getAPIData();
+    }
   }
 
   props: {
-    show: Show
+    show: Show,
+    rating: string,
+    getAPIData: Function
   };
 
   render() {
     const { title, description, year, poster, trailer } = this.props.show;
     let ratingComponent;
-    if (this.state.apiData.rating) {
+    if (this.props.rating) {
       ratingComponent = (
         <h3>
-          {this.state.apiData.rating}
+          {this.props.rating}
         </h3>
       );
     } else {
@@ -62,4 +61,17 @@ class Details extends Component {
   }
 }
 
-export default Details;
+const mapStateToProps = (state, ownProps) => {
+  const apiData = state.apiData[ownProps.show.imdbID] ? state.apiData[ownProps.show.imdbID] : {};
+  return {
+    rating: apiData.rating
+  };
+};
+
+const mapDispatchToProps = (dispatch: Function, ownProps) => ({
+  getAPIData() {
+    dispatch(getAPIDetails(ownProps.show.imdbID));
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Details);
