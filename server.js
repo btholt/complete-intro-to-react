3b602e55eba04f48e82f9e3cef0d1c2db890de27
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-15 14:41:28 -0400

    Fix hot module reloading

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 /* eslint no-console:0 */
-
 require('babel-register');
 
 const express = require('express');
@@ -8,7 +7,11 @@ const ReactDOMServer = require('react-dom/server');
 const ReactRouter = require('react-router-dom');
 const _ = require('lodash');
 const fs = require('fs');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const webpack = require('webpack');
 const App = require('./js/App').default;
+const config = require('./webpack.config');
 
 const StaticRouter = ReactRouter.StaticRouter;
 const port = 8080;
@@ -16,6 +19,14 @@ const baseTemplate = fs.readFileSync('./index.html');
 const template = _.template(baseTemplate);
 
 const server = express();
+
+const compiler = webpack(config);
+server.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+  })
+);
+server.use(webpackHotMiddleware(compiler));
 
 server.use('/public', express.static('./public'));
 
