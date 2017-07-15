4e1e7d2ccc3a18a9a0d61d76fce7d74df65842ac
Devin Morgenstern <devin.morgenstern@protonmail.com>
2017-07-15 13:38:12 -0400

    Implement server side rendering

---
+++ server.js
@@ -0,0 +1,38 @@
+/* eslint no-console:0 */
+
+require('babel-register');
+
+const express = require('express');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const ReactRouter = require('react-router-dom');
+const _ = require('lodash');
+const fs = require('fs');
+const App = require('./js/App').default;
+
+const StaticRouter = ReactRouter.StaticRouter;
+const port = 8080;
+const baseTemplate = fs.readFileSync('./index.html');
+const template = _.template(baseTemplate);
+
+const server = express();
+
+server.use('/public', express.static('./public'));
+
+server.use((req, res) => {
+  console.log(req.url);
+  const context = {};
+  const body = ReactDOMServer.renderToString(
+    React.createElement(StaticRouter, { location: req.url, context }, React.createElement(App))
+  );
+
+  if (context.url) {
+    res.redirect(context.url);
+  }
+
+  res.write(template({ body }));
+  res.end();
+});
+
+console.log(`listening on ${port}`);
+server.listen(port);
