4ed82dce44b9313dd483efa14aaf4a17a00b8c9d
matthiashaefeli <haefeli.matthias@gmail.com>
2017-07-29 19:45:28 -0500

    add eslint

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,17 +1,17 @@
 const ce = React.createElement;
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+	return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
 };
 
 const MyFirstComponent = function() {
-  return ce(
-    'div',
-    null,
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'peru' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'LimeGreen' })
-  );
+	return ce(
+		'div',
+		null,
+		ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
+		ce(MyTitle, { title: 'Stranger Things', color: 'peru' }),
+		ce(MyTitle, { title: 'Silicon Valley', color: 'LimeGreen' })
+	);
 };
 
 ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
