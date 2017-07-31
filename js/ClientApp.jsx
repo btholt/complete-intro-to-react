f81e900223169310146b96674d987f16c06e9d78
matthiashaefeli <haefeli.matthias@gmail.com>
2017-07-31 18:43:37 -0500

    add prettier in package jason

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,17 +1,17 @@
 const ce = React.createElement;
 
 var MyTitle = function(props) {
-	return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
+  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
 };
 
 var MyFirstComponent = function() {
-	return ce(
-		'div',
-		{ id: 'my-first-component' },
-		ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
-		ce(MyTitle, { color: 'peru', title: 'Orange is the New Black' }),
-		ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
-	);
+  return ce(
+    'div',
+    { id: 'my-first-component' },
+    ce(MyTitle, { color: 'rebeccapurple', title: 'House of Cards' }),
+    ce(MyTitle, { color: 'peru', title: 'Orange is the New Black' }),
+    ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
+  );
 };
 
 ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
