1a0b4758a074c4fb347c96863d5e88c3f13d7b46
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-02 18:47:28 -0500

    add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './js/ClientApp.js',
+  devtool: 'source-map',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
