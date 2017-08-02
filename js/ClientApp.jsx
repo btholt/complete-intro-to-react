9526a21844b3a83d9961ea834ae2077d40e947a4
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-01 20:01:02 -0500

    add eslint. installed also in sublime

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,10 +1,10 @@
 const ce = React.createElement;
 
-var MyTitle = function(props) {
+const MyTitle = function(props) {
 	return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
 };
 
-var MyFirstComponent = function() {
+const MyFirstComponent = function() {
 	return ce(
 		'div',
 		{ id: 'my-first-component' },
@@ -13,5 +13,7 @@ var MyFirstComponent = function() {
 		ce(MyTitle, { color: 'burlywood', title: 'Stranger Things' })
 	);
 };
+
+const x = 5;
 
 ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
