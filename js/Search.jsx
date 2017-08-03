4366e42a0eae132ccd16c93a403fd8928f452275
Zac McClung <zac.mcclung370@gmail.com>
2017-08-02 22:40:26 -0600

    Loaded JSON data and SHowCard component

---
+++ js/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
+
+
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => <ShowCard show={show} />) }
+  </div>
+);
+
+export default Search;
