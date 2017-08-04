11fc30007aac0cd5900d7eac30da121a20da5251
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-04 18:25:10 -0500

    add a rule to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,12 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      },
+      {
         test: /\.jsx?$/,
         loader: 'babel-loader'
       }
