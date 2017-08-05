adba64243e0c1af9c54fe4ef296b8e7f1e8aa569
Zac McClung <zac.mcclung370@gmail.com>
2017-08-05 16:53:44 -0600

    added shallow testing

--- js/__test__/Search.test.jsx
+++ js/__test__/Search.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Search from '../Search';
 
+// console.log(process.env.NODE_ENV);
+
 test('Search renders correctly', () => {
-  const component = renderer.create(<Search />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
 });
