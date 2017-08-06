55be9f24695969eede2dd541ecfd4e6366e4bf73
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-05 19:55:03 -0500

    add landing.jsx and changing clientApp file

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
 
 const App = () =>
-	<div className="app">
-		<div className="landing">
-			<h1>svideo</h1>
-			<input type="text" placeholder="Search" />
-			<a>or Browse All</a>
+	<HashRouter>
+		<div className="app">
+			<Route exact path="/" component={Landing} />
 		</div>
-	</div>;
-
+	</HashRouter>;
 render(<App />, document.getElementById('app'));
+
 // first version
 // const ce = React.createElement;
 
