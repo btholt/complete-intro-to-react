4df2f12190793a8e9fe2663a76fee9bd905008b8
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-06 08:58:39 -0500

    add showcard.jsx file

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
+import ShowCard from './ShowCard';
 import preload from '../data.json';
 
 const Search = () =>
 	<div className="search">
-		<div>
-			{preload.shows.map(show =>
-				<div className="show-card">
-					<img alt={`${show.title} Show Poster`} src={`/public/img/posters/${show.poster}`} />
-					<div>
-						<h3>
-							{show.title}
-						</h3>
-						<h4>
-							({show.year})
-						</h4>
-						<p>
-							{show.description}
-						</p>
-					</div>
-				</div>
-			)}
-		</div>
+		{preload.shows.map(show => <ShowCard show={show} />)}
 	</div>;
 
 export default Search;
