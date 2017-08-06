12b005f21a0a531c662f877599265e3e980fe5cf
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-06 08:25:00 -0500

    show just the title. map over array

--- js/Search.jsx
+++ js/Search.jsx
@@ -3,11 +3,18 @@ import preload from '../data.json';
 
 const Search = () =>
 	<div className="search">
-		<pre>
-			<code>
-				{JSON.stringify(preload, null, 4)}
-			</code>
-		</pre>
+		{preload.shows.map(show =>
+			<h3>
+				{show.title}
+			</h3>
+		)}
 	</div>;
 
 export default Search;
+
+// just print the data like it is
+// <pre>
+//  <code>
+//		{JSON.stringify(preload, null, 4)}}
+//	</code>
+// </pre>
