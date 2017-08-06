1f6a09befa33d4c43cdbdae26211a8e4b5f62b73
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-05 20:58:04 -0500

    add link in landing.jsx to search.jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { HashRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
+import Search from './Search';
 
 const App = () =>
 	<HashRouter>
 		<div className="app">
 			<Route exact path="/" component={Landing} />
+			<Route path="/search" component={Search} />
 		</div>
 	</HashRouter>;
 render(<App />, document.getElementById('app'));
---
+++ js/Search.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search = () => <h1>Hi this is Search</h1>;
+
+export default Search;
