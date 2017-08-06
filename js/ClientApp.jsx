785fcdc69cf0721a9c5e009f9e2230b4d0fa1f14
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-05 21:12:12 -0500

    change to browserRouter

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
 const App = () =>
-	<HashRouter>
+	<BrowserRouter>
 		<div className="app">
 			<Route exact path="/" component={Landing} />
 			<Route path="/search" component={Search} />
 		</div>
-	</HashRouter>;
+	</BrowserRouter>;
 render(<App />, document.getElementById('app'));
 
 // first version
