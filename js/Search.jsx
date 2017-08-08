676dec7885d4bde2cd3b68b1cbd891ee200d31e8
matthiashaefeli <haefeli.matthias@gmail.com>
2017-08-07 21:10:55 -0500

    changes on search.jsx class

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import ShowCard from './ShowCard';
+import React, { Component } from 'react';
 import preload from '../data.json';
+import ShowCard from './ShowCard';
 
-const Search = () =>
-	<div className="search">
-		<header>
-			<h1>svideo</h1>
-			<input type="text" placeholder="Search" />
-		</header>
-		<div>
-			{preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-		</div>
-	</div>;
+class Search extends Component {
+	render() {
+		return (
+			<div className="search">
+				<header>
+					<h1>svideo</h1>
+					<input type="text" placeholder="Search" />
+				</header>
+				<div>
+					{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Search;
 
