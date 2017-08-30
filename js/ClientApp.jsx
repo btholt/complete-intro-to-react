5d2dbb5ae06e56bbd110d0961c1ab99fa20cc530
Codelikeyouwantit <HectorFeurtado@gmail.com>
2017-08-30 15:36:37 -0400

    configuring babel

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
 const ce = React.createElement;
 
 const MyTitle = function(props) {
@@ -15,4 +18,4 @@ const MyFirstComponent = function() {
   );
 };
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById('app'));
+render(ce(MyFirstComponent), document.getElementById('app'));
