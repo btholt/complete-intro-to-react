bf5331ca9ba610adacebd80a587684994a9d4a0f
Codelikeyouwantit <HectorFeurtado@gmail.com>
2017-08-31 21:27:54 -0400

    add rules to webpack.config

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>React Video</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse all</a>
+    </div>
+  </div>
+);
 
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
