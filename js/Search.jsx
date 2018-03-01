616555b010c475a35fe290e18950e11b764c114e
SeSmC06 <dingpeng06@gmail.com>
2018-02-28 21:25:38 -0800

    learned bind properly

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-  <div className="search">
-    {preload.shows.map(show => <ShowCard show={show} />)}
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      searchTerm: 'this is some sort of debug statement'
+    };
+
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            type="text"
+            placeholder="search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => (
+            <ShowCard show={show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 export default Search;
